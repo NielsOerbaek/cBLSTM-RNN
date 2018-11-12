ce00c94cc025701f9b781e745f3c99091ca73659
Jeppeks1 <Jeppeks1@hotmail.com>
2018-11-12 13:08:28 +0100

    Merge branch 'master' of C:\Users\Jeppe\Documents\Universitet\Natural Language Processing\cBLSTM-RNN with conflicts.

--- LSTM-LM.py
+++ LSTM-LM.py
@@ -1,16 +1,15 @@
 import numpy as np
 from sklearn.utils import shuffle
 from keras.models import Sequential
-from keras.models import load_model
 from keras.layers import LSTM
-from keras.layers import GRU
 from keras.layers import Dense
 from keras.layers import TimeDistributed
+from keras.layers import Bidirectional
+from keras.layers import Embedding
+from keras.utils import to_categorical
 
 # Our Preprocessing Library
 import prepros as pp
-
-num_samples = 2000
 
 # -- Preprocessing
 # Vocab files
@@ -19,17 +18,29 @@ w2i, i2w = pp.make_vocab(pp.vocab_file)
 
 
 # Once you have generated the data files, you can outcomment the following line.
-pp.generate_data_files(num_samples)
-train_pos, train_neg, test_pos, test_neg = pp.load_all_data_files(num_samples)
+pp.generate_data_files(100)
+train_pos, train_neg, test_pos, test_neg = pp.load_all_data_files(100)
 
 # For some reason the for loop is only giving us the index here. I dunno why.
-# Convert to sentence level training set:
+# Convert to sentence level training seÂt:
 train_X = []
 for i in train_pos:
     for j in train_pos[i]:
         train_X.append(np.array(pp.words_to_ids(train_pos[i][j], w2i)))
 
 train_X = np.array(train_X)
+
+
+def to_one_hot(word_id, vocab_size):
+    v = np.full(vocab_size, 0)
+    v[word_id] = 1
+    return v
+
+
+def from_one_hot(one_hot_vector):
+    for i, v in enumerate(one_hot_vector):
+        if v == 1:
+            return i
 
 
 def dataset_to_onehot(dataset):
@@ -42,23 +53,20 @@ def dataset_to_onehot(dataset):
     for s in range(x):
         print(s/x*100)
         for w in range(y):
-            v = pp.to_one_hot(dataset[s][w], z)
-            o_h_x[s][w] = v
-            o_h_y[s][(w-1) % y] = v
+            o_h_x[s][w] = to_one_hot(dataset[s][w], z)
+            o_h_x[s][(w+1) % y] = to_one_hot(dataset[s][w], z)
     return o_h_x, o_h_y
 
 
 # Storing data to save time
-one_hot_filename = "one-hot-sentenses-1000.pickle"
+one_hot_filename = "one-hot-sentenses-100.pickle"
 generate_data = True
-if generate_data:
+if(generate_data):
     train_X, train_y = dataset_to_onehot(train_X)
     print("Saving data")
-    #pp.save_data_to_pickle(train_X, "X-" + one_hot_filename)
-    #pp.save_data_to_pickle(train_y, "Y-" + one_hot_filename)
+    pp.save_data_to_pickle((train_X, train_y), one_hot_filename)
 else:
-    train_X = pp.load_data_from_pickle("X-" + one_hot_filename)
-    train_y = pp.load_data_from_pickle("Y-" + one_hot_filename)
+    (train_X, train_y) = pp.load_data_from_pickle(one_hot_filename)
 
 print(train_X.shape)
 print(train_y.shape)
@@ -75,28 +83,33 @@ def perplexity(review):
     return product**(-1/size)
 
 
-model_name = "./model/lstm-pos-lm-" + str(pp.vocab_size) + "vocab-" + str(num_samples) + "reviews-max-length-" + str(pp.max_sent_length) + ".model"
-generate_model = True
-if generate_model:
-    # define LSTM
-    print("Creating model")
-    model = Sequential()
-    model.add(LSTM(pp.max_sent_length, input_shape=(pp.max_sent_length, pp.vocab_size), return_sequences=True, dropout=0.2))
-    model.add(TimeDistributed(Dense(pp.vocab_size, activation="sigmoid")))
-    model.compile(loss='categorical_crossentropy', optimizer='adam')
-    print(model.summary())
-    # train LSTM
-    model.fit(train_X, train_y, epochs=10, batch_size=100, verbose=1)
+# define LSTM
+print("Creating model")
+model = Sequential()
+model.add(LSTM(pp.max_sent_length, input_shape=(pp.max_sent_length, pp.vocab_size), return_sequences=True))
+model.add(TimeDistributed(Dense(pp.vocab_size)))
+model.compile(loss='mean_squared_error', optimizer='adam')
+print(model.summary())
+# train LSTM
+model.fit(train_X, train_y, epochs=5, batch_size=100, verbose=1)
 
-    model.save(model_name)
-else:
-    model = load_model(model_name)
+model.save("./model/lstm-pos-lm-2003vocab-100reviews-max-length-100.model")
+
+print(model.predict(train_X[0:2]))
 
 
-predictions = model.predict(train_X[0:20])
+# Jeppe attempt to improve the above code:
 
-for k in range(len(predictions)):
-    print("Input:", pp.one_hots_to_sentence(train_X[k], i2w))
-    print("Correct:", pp.one_hots_to_sentence(train_y[k], i2w))
-    print("Prediction", pp.one_hots_to_sentence(predictions[k], i2w))
-    print("------------------")
+# define LSTM
+print("Creating model")
+model = Sequential()
+model.add(LSTM(pp.max_sent_length, input_shape=(pp.max_sent_length, pp.vocab_size), return_sequences=True))
+model.add(TimeDistributed(Dense(pp.vocab_size)))
+model.compile(loss='mean_squared_error', optimizer='adam')
+print(model.summary())
+# train LSTM
+model.fit(train_X, train_y, epochs=5, batch_size=100, verbose=1)
+
+model.save("./model/lstm-pos-lm-2003vocab-100reviews-max-length-100.model")
+
+print(model.predict(train_X[0:2]))
